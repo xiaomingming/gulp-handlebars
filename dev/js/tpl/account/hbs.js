7c593895d07f9d58e329af946c53ec80979de2c0
leweiming <leweiming@youku.com>
2016-10-09 17:17:29 +0800

    修改打包非标准流导致的bug

--- dev/js/tpl/account/hbs.js
+++ dev/js/tpl/account/hbs.js
@@ -1,6 +1,8 @@
-define(["runtime"], function(Handlebars) {this["Vip"] = this["Vip"] || {};
-this["Vip"]["account"] = this["Vip"]["account"] || {};
-this["Vip"]["account"]["main"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
+define(["runtime","helpers"],function(Handlebars,helpers){
+
+this["myApp"] = this["myApp"] || {};
+this["myApp"]["account"] = this["myApp"]["account"] || {};
+this["myApp"]["account"]["main"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
   var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
   return "			<li>"
     + escapeExpression(((helper = (helper = helpers.desc || (depth0 != null ? depth0.desc : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"desc","hash":{},"data":data}) : helper)))
@@ -14,9 +16,12 @@ this["Vip"]["account"]["main"] = Handlebars.template({"1":function(depth0,helper
   if (stack1 != null) { buffer += stack1; }
   return buffer + "	</ul>\r\n</div>";
 },"usePartial":true,"useData":true});
-Handlebars.registerPartial("account", Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+Handlebars.registerPartial("account/_account", this["myApp"]["account"]["_account"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
   var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
   return "<h2>"
     + escapeExpression(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"title","hash":{},"data":data}) : helper)))
     + "</h2>\r\n";
-},"useData":true}));return this["Vip"];});
+},"useData":true}));
+return this["myApp"];
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -8,18 +8,10 @@ var path = require('path');
 var fs = require('fs');
 var clean = require('gulp-clean');
 var through2 = require('through2');
+var defineModule = require('gulp-define-module');
+var wrapAmd = require('gulp-wrap-amd');
 var tplBaseDir = 'dev/js/tpl/';
-var hbsNamespace = 'Vip'; //hbs命名空间
-var moduleWrapConfig = {
-    amd: {
-        start: 'define(["runtime"], function(Handlebars) {',
-        end: 'return this["' + hbsNamespace + '"];});'
-    },
-    cmd: {
-        start: 'module.exports = {',
-        end: 'return this["' + hbsNamespace + '"];};'
-    }
-};
+var hbsNamespace = 'myApp'; //hbs命名空间
 // 获取文件目录
 function getFolders(dir) {
     return fs.readdirSync(dir)
@@ -48,7 +40,9 @@ function taskLoop(basedir, taskfn) {
     return task;
 }
 gulp.task('del-hbs', function() {
-    return gulp.src([tplBaseDir + '**/*.js'], { read: false }).pipe(clean());
+    return gulp.src([tplBaseDir + '**/*.js'], {
+        read: false
+    }).pipe(clean());
 });
 gulp.task('hbs-compile', ['del-hbs'], function() {
     var hbsTask = taskLoop(tplBaseDir, function(folder) {
@@ -59,7 +53,7 @@ gulp.task('hbs-compile', ['del-hbs'], function() {
             }))
             .pipe(wrap('Handlebars.template(<%= contents %>)'))
             .pipe(declare({
-                namespace: 'Vip',
+                namespace: hbsNamespace,
                 noRedeclare: true,
                 processName: function(filePath) {
                     //获取文件上级目录和文件名称，比如 account/main.js
@@ -75,10 +69,10 @@ gulp.task('hbs-compile', ['del-hbs'], function() {
             .pipe(handlebars({
                 handlebars: require('handlebars')
             }))
-            .pipe(wrap('Handlebars.registerPartial(<%= processPartialName(file.relative) %>, Handlebars.template(<%= contents %>));', {}, {
+            .pipe(wrap('Handlebars.registerPartial(<%= processPartialName(file.relative) %>, this["' + hbsNamespace + '"][<%= processPartialName(file.relative).split("/")[0] %>"]["<%= processPartialName(file.relative).split("/")[1] %>] = Handlebars.template(<%= contents %>));', {}, {
                 imports: {
                     processPartialName: function(fileName) {
-                        return JSON.stringify(path.basename(fileName, '.js').substr(1));
+                        return JSON.stringify(folder + '/' + path.basename(fileName, '.js'));
                     }
                 }
             }))
@@ -86,21 +80,54 @@ gulp.task('hbs-compile', ['del-hbs'], function() {
     });
     return merge(hbsTask, partialTask);
 });
-
-gulp.task('hbs-merge', ['hbs-compile'], function() {
+gulp.task('hbs-partials', function() {
+    var spath = tplBaseDir + '/_*.hbs';
+    return gulp.src(spath)
+        .pipe(handlebars({
+            handlebars: require('handlebars')
+        }))
+        .pipe(wrap('Handlebars.registerPartial(<%= processPartialName(file.relative) %>, Handlebars.template(<%= contents %>));', {}, {
+            imports: {
+                processPartialName: function(fileName) {
+                    return JSON.stringify(path.basename(fileName, '.js'));
+                }
+            }
+        }))
+        .pipe(gulp.dest(tplBaseDir));
+});
+gulp.task('hbs-merge', ['hbs-compile', 'hbs-partials'], function() {
     var task = taskLoop(tplBaseDir, function(folder) {
-        return gulp.src(path.join(tplBaseDir, folder, '/*[^hbs].js'))
-            .pipe(concat('entry.js'))
+        return gulp.src([path.join(tplBaseDir, folder, '/[^_]*.js'), path.join(tplBaseDir, folder, '/_*.js'), tplBaseDir + '/_*.js'])
+            .pipe(concat('hbs.js'))
             .pipe(gulp.dest(path.join(tplBaseDir, folder)));
     });
     return merge(task);
 });
 gulp.task('ctpl', ['hbs-merge'], function() {
-    return gulp.src([tplBaseDir + '**/*.js', '!' + tplBaseDir + '**/entry.js'], { read: false }).pipe(clean());
+    return gulp.src([tplBaseDir + '**/*.js', '!' + tplBaseDir + '**/hbs.js', '!' + tplBaseDir + '/*.js'], {
+        read: false
+    }).pipe(clean());
 });
-gulp.task('hbs', ['ctpl'], function() {
-    taskLoop(tplBaseDir, function(folder) {
-        var filePath = path.join(tplBaseDir, folder, '/entry.js');
-        return hbsAmdWrap(filePath, path.join(tplBaseDir, folder, '/hbs.js'));
+gulp.task('hbs-common', function() {
+    var spath = tplBaseDir + '/[^_]*.hbs';
+    return gulp.src(spath)
+        .pipe(handlebars())
+        .pipe(defineModule('amd', {
+            require: {
+                Handlebars: 'runtime',
+                helpers: 'helpers'
+            }
+        }))
+        .pipe(gulp.dest(tplBaseDir));
+});
+gulp.task('hbs', ['ctpl', 'hbs-common'], function() {
+    var task = taskLoop(tplBaseDir, function(folder) {
+        var filePath = path.join(tplBaseDir, folder, '/hbs.js');
+        return gulp.src(filePath).pipe(wrapAmd({
+            deps: ['runtime', 'helpers'],
+            params: ['Handlebars', 'helpers'],
+            exports: 'this["' + hbsNamespace + '"]',
+        })).pipe(gulp.dest(tplBaseDir + '/' + folder));
     });
+    return merge(task);
 });
